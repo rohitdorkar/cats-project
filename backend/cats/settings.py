c51214079ad10d1efa0f5272b598ccd57b62fcc9
rohitdorkar <rohitdorkar09>
2026-05-04 12:50:01 +0530

    Add render.yaml - force Python 3.11

--- backend/cats/settings.py
+++ backend/cats/settings.py
@@ -212,6 +212,7 @@ if DATABASE_URL:
             DATABASE_URL,
             conn_max_age=600,
             ssl_require=True,
+            engine='django.db.backends.postgresql',
         )
     }
 else:
